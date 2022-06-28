e71622c1ce5b21bd115c21fef1abdcac3f3d59bc
unknown <lanskeit6@gmail.com>
2022-06-28 22:32:53 +0100

    No more code smell

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,14 +1,17 @@
 package com.mycompany.app;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	public static final Logger LOGGER = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-	System.out.println("ceci est un message de test");
+        LOGGER.info( "Hello World!" );
+        LOGGER.info("ceci est un message de test");
     }
 }
